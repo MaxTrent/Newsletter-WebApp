7535c9f4ecc0553d6d2283dce2d47713d81b5b67
Samuel Adelowo <samueladelowo92@gmail.com>
2023-02-25 14:50:52 +0100

    set up app

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const axios = require("axios");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.post('/');
+app.post("/", (req, res) => {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+  console.log(firstName, lastName, email);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
 });
+
+
+// 33c5d1f4be7457dcdded9c3cf52a2ace-us18
