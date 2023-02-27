731c8bb2a88962577e35f0441e25bf389fdd514a
Samuel Adelowo <samueladelowo92@gmail.com>
2023-02-27 21:43:06 +0100

    deploying the app

--- app.js
+++ app.js
@@ -25,18 +25,18 @@ app.post("/", (req, res) => {
       skip_merge_validation: false,
       skip_duplicate_check: false,
     },
-    data: {
-      members: [
-        {
-          email_address: email,
-          status: "subscribed",
-          merge_fields: {
-            FNAME: firstName,
-            LNAME: lastName,
-          },
-        },
-      ],
-    },
+    // data: {
+    //   members: [
+    //     {
+    //       email_address: email,
+    //       status: "subscribed",
+    //       merge_fields: {
+    //         FNAME: firstName,
+    //         LNAME: lastName,
+    //       },
+    //     },
+    //   ],
+    // },
     headers: {
       Authorization: "Bearer ",
     },
@@ -45,16 +45,24 @@ app.post("/", (req, res) => {
     .then((response) => {
       resp = response.data;
       console.log(response.status);
+      res.sendFile(__dirname + "/success.html");
     })
     .catch((err) => {
       console.log(err.response.status);
+
+      res.sendFile(__dirname + "/failure.html");
     });
   //   console.log(firstName, lastName, email);
+});
+
+app.post("/failure", (req, res) => {
+  res.redirect("/");
 });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
 });
 
+// 9ff49a75fb45476c5cf5f617d8e7a9a3-us18
 
 // b43a2d8598
